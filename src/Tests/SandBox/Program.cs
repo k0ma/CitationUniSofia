94c9224382b74a55dff04ca1ae875c1dc618a71f
k0ma <iv.kameliya@gmail.com>
2019-09-22 11:11:50 +0300

    Create SandBox app for testing

---
+++ src/Tests/SandBox/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace SandBox
+{
+    using CitationUniSofia.Data;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"{typeof(Program).Namespace} ({string.Join(" ", args)}) starts working...");
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider(true);
+
+            // Seed data on application startup
+            //using (var serviceScope = serviceProvider.CreateScope())
+            //{
+            //    var dbContext = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            //    dbContext.Database.Migrate();
+            //    new ApplicationDbContextSeeder().SeedAsync(dbContext, serviceScope.ServiceProvider).GetAwaiter().GetResult();
+            //}
+
+            using (var serviceScope = serviceProvider.CreateScope())
+            {
+                serviceProvider = serviceScope.ServiceProvider;
+
+                SandboxCode(serviceProvider);
+            }
+        }
+
+        private static void SandboxCode(IServiceProvider serviceProvider)
+        {
+            var db = serviceProvider.GetService<CitationUniSofiaContext>();
+            Console.WriteLine(db.Users.Count());
+        }
+
+        private static void ConfigureServices(ServiceCollection serviceCollection)
+        {
+
+            var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            serviceCollection.AddDbContext<CitationUniSofiaContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection")));
+
+        }
+    }
+}
